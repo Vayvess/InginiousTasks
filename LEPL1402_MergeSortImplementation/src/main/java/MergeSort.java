fdff083d9ef8b25872992d9c37cf263457921d0c
BarghestEPL <BarghestEPL@gmail.com>
2020-01-02 18:11:13 +0100

    [Module 2] Merge Sort Implementation 100% Done !

---
+++ LEPL1402_MergeSortImplementation/src/main/java/MergeSort.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.Map;
+
+public class MergeSort {
+
+    /**
+     * Pre-conditions: a[lo..mid] and a[mid+1..hi] are sorted
+     * Post-conditions: a[lo..hi] is sorted
+     */
+    private static void merge(int[] a, int[] aux, int lo, int mid, int hi) {
+        for (int k = lo; k <= hi; k++) {
+            aux[k] = a[k];
+        }
+
+        int i = lo, j = mid+1;
+        for (int k = lo; k <= hi; k++) {
+            if(i > mid) {
+                a[k] = aux[j++];
+            }else if (j > hi){
+                a[k] = aux[i++];
+            }else if (aux[j]< aux[i]){
+                a[k] = aux[j++];
+            }else{
+                a[k] = aux[i++];
+            }
+        }
+    }
+    /**
+     * Rearranges the array in ascending order, using the natural order
+     */
+    public static void sort(int[] a) {
+        int[] aux = new int[a.length];
+        sort(a, aux, 0, a.length-1);
+    }
+
+    private static void sort(int[] a, int[] aux, int lo, int hi) {
+        if (hi <= lo) return;
+        int mid = lo + (hi - lo) / 2;
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
+    }
+
+    public static void main(String[] args) {
+        int size = 15;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(array));
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
